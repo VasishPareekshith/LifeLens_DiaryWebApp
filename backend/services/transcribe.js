62de0329f3930878daab3444ce749b1b27fde0a1
Vasish Pareekshith S S <146262641+VasishPareekshith@users.noreply.github.com>
2024-12-11 06:43:37 +0530

    Update transcribe.js

--- backend/services/transcribe.js
+++ backend/services/transcribe.js
@@ -1,25 +1,24 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-    accessKeyId:'###',
-    secretAccessKey:'###',
-    region: '###'
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 
 const s3 = new AWS.S3();
 const transcribeService = new AWS.TranscribeService();
 
-// S3 Bucket Name
 const BUCKET_NAME = 'lifelens-audio'; // Replace with your actual S3 bucket name
 
-// Function to upload audio file to S3
+
 const uploadToS3 = async (fileBuffer, fileName) => {
     const s3Key = `audio/${fileName}`; // Use the file name directly
 
     const params = {
         Bucket: BUCKET_NAME,
         Key: s3Key,
-        Body: fileBuffer, // This should be populated
+        Body: fileBuffer, 
         ContentType: 'audio/m4a', // Adjust according to your audio type
     };
 
@@ -33,7 +32,6 @@ const uploadToS3 = async (fileBuffer, fileName) => {
     }
 };
 
-// Function to start a transcription job
 const startTranscriptionJob = async (jobName, mediaUri) => {
     const params = {
         TranscriptionJobName: jobName,
@@ -48,14 +46,13 @@ const startTranscriptionJob = async (jobName, mediaUri) => {
     try {
         const data = await transcribeService.startTranscriptionJob(params).promise();
         console.log('Transcription job started:', data);
-        return data; // Return job information
+        return data; 
     } catch (error) {
         console.error('Error starting transcription job:', error);
-        throw error; // Rethrow the error for further handling
+        throw error; 
     }
 };
 
-// Function to get the status of a transcription job
 const getTranscriptionJob = async (jobName) => {
     const params = {
         TranscriptionJobName: jobName,
@@ -64,10 +61,10 @@ const getTranscriptionJob = async (jobName) => {
     try {
         const data = await transcribeService.getTranscriptionJob(params).promise();
         console.log('Transcription job status:', data);
-        return data; // Return job status
+        return data;
     } catch (error) {
         console.error('Error getting transcription job status:', error);
-        throw error; // Rethrow the error for further handling
+        throw error;
     }
 };
 
